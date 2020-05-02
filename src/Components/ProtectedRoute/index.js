9a92adc2750f9b4648c9252b8a00a180bcef5834
Sheriff <danmbugua74@gmail.com>
2020-05-02 12:13:30 +0300

    Add location date picker

---
+++ src/Components/ProtectedRoute/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Redirect } from 'react-router-dom'
+import Layout  from '../Layout'
+import Auth from '../Auth'
+
+export const ProtectedRoute = ({component: Component, ...rest}) => {
+  return (
+    <Layout {...rest} render={
+      (props) => {
+        if (1 == 2) {
+          return <Component {...props}/>
+        }
+        else {
+          return <Redirect to={
+            {
+              pathname: "/auth",
+              state: {
+                from: props.location
+              }
+            }
+          }/>
+        }
+      }
+    }/>
+  )
+}
